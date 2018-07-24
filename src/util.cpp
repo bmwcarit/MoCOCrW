fa0600b8dc8d3c0dd31ef0fada6e5b5b1d4d1cc2
Oleksii Miroshko <Oleksii.Miroshko@bmw.de>
2018-07-24 23:57:55 +0200

    Add toHex function to util module

--- src/util.cpp
+++ src/util.cpp
@@ -16,20 +16,22 @@
  * limitations under the License.
  * #L%
  */
-#pragma once
-
-#include <memory>
-#include <vector>
+#include "mococrw/util.h"
+#include <sstream>
+#include <iomanip>
 
 namespace mococrw
 {
 namespace utility
 {
 
-template <class T>
-using SharedPtrTypeFromUniquePtr = std::shared_ptr<typename T::element_type>;
+std::string toHex(const std::vector<uint8_t> &data) {
+    std::stringstream result;
+    for(int i = 0, size = data.size(); i < size; i++) {
+        result << std::hex << std::setfill('0') << std::setw(2) << (int)data[i];
+    }
+    return result.str();
+}
 
-std::string toHex(std::vector<uint8_t> &data);
-
-}  //::utility
-}  //::mococrw
+}
+}
