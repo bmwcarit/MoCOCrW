dc0e210364f1ac3b9a987a9f0cf3e0d7961d758c
info@hpsware.com <Marco.Czech@partner.bmwgroup.com>
2019-07-09 16:02:16 +0200

    mococrw: add attribute order feature for subject
    This allows users to use custom subject attribute order for X509 name.

--- tests/unit/test_distinguished_name.cpp
+++ tests/unit/test_distinguished_name.cpp
@@ -101,3 +101,174 @@ TEST_F(DistinguishedNameTest, testThatDistinguishedNameIsPopulatedCorrectly)
 
     ASSERT_THAT(dn, Eq(_dn));
 }
+
+TEST_F(DistinguishedNameTest, testThatOldBuilderKeepsOldOrder) {
+    auto builder = DistinguishedName::Builder();
+    builder.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("oben")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan")
+            .countryName("US")
+            .countryName("CH");
+    auto dn = builder.build();
+    auto x509Name = _X509_NAME_new();
+    dn.populateX509Name(x509Name);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::CommonName)[0], 0);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::CountryName)[0], 1);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::LocalityName)[0], 2);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::StateOrProvinceName)[0], 3);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::OrganizationalUnitName)[0], 4);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::OrganizationName)[0], 5);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::Pkcs9EmailAddress)[0], 6);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::SerialNumber)[0], 7);
+    auto entry = _X509_NAME_get_entry(x509Name.get(), 1);
+    auto name = _X509_NAME_ENTRY_get_data(entry);
+    ASSERT_EQ(name, "CH");
+}
+
+TEST_F(DistinguishedNameTest, testThatCustomAttributeOrderWorks) {
+    auto builder = DistinguishedName::CustomOrderBuilder();
+    builder.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("oben")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn = builder.build();
+    auto x509Name = _X509_NAME_new();
+    dn.populateX509Name(x509Name);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::CommonName)[0], 0);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::CountryName)[0], 1);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::Pkcs9EmailAddress)[0], 2);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::OrganizationalUnitName)[0], 3);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::LocalityName)[0], 4);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::SerialNumber)[0], 5);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::OrganizationName)[0], 6);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::StateOrProvinceName)[0], 7);
+}
+
+TEST_F(DistinguishedNameTest, testThatCustomAttributeWithDuplicatesWorks) {
+    auto builder = DistinguishedName::CustomOrderBuilder();
+    builder.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("oben")
+            .localityName("links")
+            .localityName("rechts")
+            .localityName("unten")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn = builder.build();
+    auto x509Name = _X509_NAME_new();
+    dn.populateX509Name(x509Name);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::CommonName)[0], 0);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::CountryName)[0], 1);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::Pkcs9EmailAddress)[0], 2);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::OrganizationalUnitName)[0], 3);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::LocalityName)[0], 4);
+    ASSERT_EQ(_X509_NAME_ENTRY_get_data(_X509_NAME_get_entry(x509Name.get(), 4)), "oben");
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::LocalityName)[1], 5);
+    ASSERT_EQ(_X509_NAME_ENTRY_get_data(_X509_NAME_get_entry(x509Name.get(), 5)), "links");
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::LocalityName)[2], 6);
+    ASSERT_EQ(_X509_NAME_ENTRY_get_data(_X509_NAME_get_entry(x509Name.get(), 6)), "rechts");
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::LocalityName)[3], 7);
+    ASSERT_EQ(_X509_NAME_ENTRY_get_data(_X509_NAME_get_entry(x509Name.get(), 7)), "unten");
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::SerialNumber)[0], 8);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::OrganizationName)[0], 9);
+    ASSERT_EQ(_X509_NAME_get_index_by_NID(x509Name.get(), openssl::ASN1_NID::StateOrProvinceName)[0], 10);
+}
+
+TEST_F(DistinguishedNameTest, testThatComparsionWorksWithOldBuilder) {
+    auto builder1 = DistinguishedName::Builder();
+    builder1.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("unten")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn1 = builder1.build();
+    auto builder2 = DistinguishedName::Builder();
+    builder2.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("unten")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn2 = builder2.build();
+    // same order and same data so expect equal
+    ASSERT_EQ(dn1, dn2);
+    auto builder3 = DistinguishedName::Builder();
+    builder3.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .localityName("unten")
+            .organizationalUnitName("Linux Support")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn3 = builder3.build();
+    // order has changed still expect equal
+    ASSERT_EQ(dn2, dn3);
+}
+
+TEST_F(DistinguishedNameTest, testThatComparsionWorksWithCustomOrderBuilder) {
+    auto builder1 = DistinguishedName::CustomOrderBuilder();
+    builder1.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("unten")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn1 = builder1.build();
+    auto builder2 = DistinguishedName::CustomOrderBuilder();
+    builder2.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .organizationalUnitName("Linux Support")
+            .localityName("unten")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn2 = builder2.build();
+    // same order and same data so expect equal
+    ASSERT_EQ(dn1, dn2);
+    auto builder3 = DistinguishedName::CustomOrderBuilder();
+    builder3.commonName("ImATeapot")
+            .countryName("DE")
+            .pkcs9EmailAddress("support@example.com")
+            .localityName("unten")
+            .organizationalUnitName("Linux Support")
+            .serialNumber("08E36DD501941432358AFE8256BC6EFD")
+            .organizationName("Linux AG")
+            .stateOrProvinceName("nebenan");
+    auto dn3 = builder3.build();
+    // order has changed so expect not equal
+    ASSERT_NE(dn2, dn3);
+}
+
+
+TEST_F(DistinguishedNameTest, testThatComparsionWorksWithMixedBuilders) {
+    auto builder1 = DistinguishedName::CustomOrderBuilder();
+    builder1.countryName("DE")
+            .commonName("ImATeapot");
+    auto dn1 = builder1.build();
+    auto builder2 = DistinguishedName::Builder();
+    builder2.commonName("ImATeapot")
+            .countryName("DE");
+    auto dn2 = builder2.build();
+    ASSERT_EQ(dn1 != dn2, dn2 != dn1);
+}
