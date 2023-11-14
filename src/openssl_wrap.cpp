1563b090a5cc337bb14385222afe2df92058a8c8
cps-b <136316734+cps-b@users.noreply.github.com>
2023-11-14 16:09:39 +0100

    convert to char* instead of const char*

--- src/openssl_wrap.cpp
+++ src/openssl_wrap.cpp
@@ -520,7 +520,7 @@ const std::array<OSSL_PARAM, 4> _getOSSLParamFromDigestType(DigestTypes type)
     }
 
     OSSL_PARAM params[4], *p = params;
-    *p++ = lib::OpenSSLLib::SSL_OSSL_PARAM_construct_utf8_string(OSSL_MAC_PARAM_DIGEST, digest_name.c_str(), sizeof(digest_name.c_str()));
+    *p++ = lib::OpenSSLLib::SSL_OSSL_PARAM_construct_utf8_string(OSSL_MAC_PARAM_DIGEST, const_cast<char*>(digest_name.c_str()), sizeof(digest_name.c_str()));
     *p = lib::OpenSSLLib::SSL_OSSL_PARAM_construct_end();
 
     std::array<int, 4> ossl_params;
